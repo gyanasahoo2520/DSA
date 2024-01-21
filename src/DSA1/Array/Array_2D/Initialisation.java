f7b37261123f73e7ab130d90d6d21f2dbd2680ea
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2024-01-21 23:21:39 +0530

    commit

---
+++ src/DSA1/Array/Array_2D/Initialisation.java
@@ -0,0 +1,25 @@
+package DSA1.Array.Array_2D;
+
+import java.util.Scanner;
+
+public class Initialisation {
+    public static void main(String[] args) {
+        int[][] arr={{10,20,30,40},
+                {50,60,70,80},
+                {90,100,110,120},
+                {130,140,150,160}};
+        System.out.println("The Wave-Matrix is :");
+        for (int i=0;i<4;i++){
+            if(i%2==0){
+                for (int j=0;j<4;j++){
+                    System.out.print(arr[i][j]+" ");
+                }
+            }
+            else
+                for (int j=3;j>=0;j--){
+                    System.out.print(arr[i][j]+" ");
+                }
+            System.out.println();
+        }
+    }
+}
---
+++ src/DSA1/Array/Array_2D/WaveForm.java
@@ -0,0 +1,7 @@
+package DSA1.Array.Array_2D;
+//Printing the Matrix in Wave form
+public class WaveForm {
+    public static void main(String[] args) {
+
+    }
+}
