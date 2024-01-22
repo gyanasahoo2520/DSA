05b6332a67571497e825ae7f4b8debffcba7c456
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2024-01-22 09:08:10 +0530

    commit

--- src/DSA1/Array/Array7_Greatercount.java
+++ src/DSA1/Array/Array7_Greatercount.java
@@ -16,7 +16,7 @@ public class Array7_Greatercount {
     }
     public static int Countgreater(int arr[]){
         int max=arr[0];
-        for (int i=0;i< arr.length;i++){
+        for (int i=1;i< arr.length;i++){
             if (arr[i]>max)
                 max=arr[i];
         }
