002d9f24e6ec6a9b9ad48669e3571a7bebdd2918
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2024-01-29 23:57:00 +0530

    sort commit

---
+++ src/DSA1/Array/Sorting/CyclicSort.java
@@ -0,0 +1,24 @@
+package DSA1.Array.Sorting;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr= {3,5,2,1,4};
+        cyclic(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void cyclic(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i]-1;
+            if(arr[i] != arr[correct]){
+                int temp=arr[i];
+                arr[i]=arr[correct];
+                arr[correct]=temp;
+            }else{
+                i++;
+            }
+        }
+    }
+}
