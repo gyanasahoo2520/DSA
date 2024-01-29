bd645574812d09b71ce67218f21d422ab5593ece
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2024-01-29 23:57:29 +0530

    sort commit

--- src/DSA1/Array/Array3_Max.java
+++ src/DSA1/Array/Array3_Max.java
@@ -19,7 +19,7 @@ public class Array3_Max {
 //            else
 //                max=arr[i+1];
 //            }
-        for (int i=0;i<n;i++){
+        for (int i=1;i<n;i++){
             if(arr[i]>max){
                 max=arr[i];
             }
