e83e7d9460846dcf5e4fb9358762e3efab38a05d
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2024-02-05 00:28:11 +0530

    Recursion commit

---
+++ src/DSA1/Array/Recursion/Fact.java
@@ -0,0 +1,24 @@
+package DSA1.Array.Recursion;
+
+public class Fact {
+    public static void main(String[] args) {
+        // Factorial of a number
+//        int ans=fact(5);
+//        System.out.println(ans);
+        // sum of first n natural number
+        int Sum=sum(5);
+        System.out.println(Sum);
+    }
+    static int fact(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+    static int sum(int n){
+        if(n==1){
+            return 1;
+        }
+        return n+sum(n-1);
+    }
+}
---
+++ src/DSA1/Array/Recursion/RevCall.java
@@ -0,0 +1,33 @@
+package DSA1.Array.Recursion;
+
+public class RevCall {
+    public static void main(String[] args) {
+        // print 5 to 1
+//        print(5);
+        // print 1 to 5 when N=5
+//        printRev(5);
+        concept(5);
+    }
+    static void print(int n){
+        if(n==0)
+            return;
+        System.out.println(n);
+        print(n-1);
+    }
+    static void printRev(int n){
+        if(n==0)
+            return;
+        printRev(n-1);
+        System.out.println(n);
+    }
+    static void concept(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        //n-- vs --n
+//        concept(n--);
+        concept(--n);
+
+    }
+}
